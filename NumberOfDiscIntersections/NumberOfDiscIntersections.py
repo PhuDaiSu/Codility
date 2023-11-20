914adf0e63169906e55c9494543392c60cc733f7
Justin Vo-Hoang <PhuDaiSu@users.noreply.github.com>
2023-11-20 22:48:10 +0700

    Task Score: 62% | Correctness Score: 100%

---
+++ NumberOfDiscIntersections/NumberOfDiscIntersections.py
@@ -0,0 +1,20 @@
+def solution(A):
+    size = len(A)
+    result = 0
+    for i in range(size):
+        a = A[i]
+        if i + a >= size - 1:
+            result += size - 1 - i
+            if result >= 10_000_000:
+                return -1
+        else:
+            result += a
+            if result >= 10_000_000:
+                return -1
+            fromJ = i + a + 1
+            for j in range(fromJ, size):
+                if A[j] > j - fromJ:
+                    result += 1
+                    if result >= 10_000_000:
+                        return -1
+    return result
