a6916a460eb96e979ed7ed5c265be8c8677f433b
Justin Vo-Hoang <PhuDaiSu@users.noreply.github.com>
2023-11-21 17:35:16 +0700

    Task Score: 87% | Correctness Score: 87%

---
+++ NumberOfDiscIntersections/NumberOfDiscIntersections.kt
@@ -0,0 +1,29 @@
+// import kotlin.math.*
+
+fun solution(A: IntArray): Int {
+    val size = A.size
+    var result = 0
+
+    for (i in 0 until size) {
+        val a = A[i]
+
+        if (i + a >= size - 1) {
+            result += size - 1 - i
+        } else {
+            result += a
+            val fromJ = i + a + 1
+
+            for (j in fromJ until size) {
+                if (A[j] > j - fromJ) {
+                    result += 1
+                }
+            }
+        }
+
+        if (result > 10_000_000) {
+            return -1
+        }
+    }
+
+    return result
+}
