6227159db67ce647a5b982cf821c303a7d664319
Justin Vo-Hoang <PhuDaiSu@users.noreply.github.com>
2023-11-21 18:33:43 +0700

    Task Score: 87% | Correctness Score: 87%

--- NumberOfDiscIntersections/NumberOfDiscIntersections.kt
+++ NumberOfDiscIntersections/NumberOfDiscIntersections.kt
@@ -1,33 +1,22 @@
 fun solution(A: IntArray): Int {
     val size = A.size
     var result = 0
-
     for (i in 0 until size) {
         val a = A[i]
-
-        if (i + a >= size - 1) {
+        if ((i + a).toLong() >= size - 1) {
             result += size - 1 - i
-            if (result > 10_000_000) {
-                return -1
-            }
         } else {
             result += a
             val fromJ = i + a + 1
-
             for (j in fromJ until size) {
                 if (A[j] > j - fromJ) {
                     result += 1
-                    if (result > 10_000_000) {
-                        return -1
-                    }
                 }
             }
         }
-
         if (result > 10_000_000) {
             return -1
         }
     }
-
     return result
 }
