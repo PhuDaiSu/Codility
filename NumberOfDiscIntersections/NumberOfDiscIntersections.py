d926abaca09bb0316278564a9a5715132907d298
Justin Vo-Hoang <PhuDaiSu@users.noreply.github.com>
2023-11-21 19:44:07 +0700

    Task Score: 62% | Correctness Score: 100%

--- NumberOfDiscIntersections/NumberOfDiscIntersections.py
+++ NumberOfDiscIntersections/NumberOfDiscIntersections.py
@@ -1,12 +1,6 @@
 def solution(A):
     size = len(A)
-    sortedA = sorted(A)
     result = 0
-
-    for i in range(len(sortedA)):
-        
-
-
     for i in range(size):
         a = A[i]
         if i + a >= size - 1:
@@ -17,6 +11,6 @@ def solution(A):
             for j in range(fromJ, size):
                 if A[j] > j - fromJ:
                     result += 1
-        if result > 10_000_000:
+        if result >= 10_000_000:
             return -1
     return result
